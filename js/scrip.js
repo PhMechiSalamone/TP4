1f449fdadad10a43d5c39ce80fef65b36e10d4af
mercedes <ph.mechi.salamone@gmail.com>
2023-05-05 23:14:20 -0300

    entrega 4

---
+++ js/scrip.js
@@ -0,0 +1,14 @@
+
+
+
+alert("Wellcome, log in!")
+
+let houses = prompt ('escribe tu casa')
+let nombre = prompt ('escribe tu nombre')
+let espacioBlanco= ' '
+
+
+let usuario = 'Wellcome' + espacioBlanco + nombre  + espacioBlanco + 'to' +  espacioBlanco +houses
+
+console.log(usuario)
+
